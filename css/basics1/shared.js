8bcb5905280a4fd51ddb6aeb1a1fa36b3d3481a2
Pablo Silva <pablo.silva@dataart.com>
2023-03-09 13:08:12 -0300

    animations

--- css/basics1/shared.js
+++ css/basics1/shared.js
@@ -4,12 +4,13 @@ var selectPlanButtons = document.querySelectorAll(".plan button");
 var modalNoButton = document.querySelector(".modal__action--negative");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
     // modal.style.display = "block";
-    // backdrop.style.display = "block";
     backdrop.classList.add("open");
+    backdrop.style.display = "block";
     modal.classList.add("open");
   });
 }
@@ -24,9 +25,25 @@ backdrop.addEventListener("click", function () {
 function closeModal() {
   modal && modal.classList.remove("open");
   backdrop.classList.remove("open");
+  setTimeout(function () {
+    backdrop.style.display = "none";
+  },200);//delay has to match at least the transition
 }
 
 toggleButton.addEventListener("click", function () {
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(function () {
+    backdrop.classList.add("open");
+  },10);
 });
+
+ctaButton.addEventListener('animationstart', function(event){
+  console.log('animation started', event)
+})
+ctaButton.addEventListener('animationend', function(event){
+  console.log('animation ended', event)
+})
+ctaButton.addEventListener('animationiteration', function(event){
+  console.log('animation iterated', event)
+})
